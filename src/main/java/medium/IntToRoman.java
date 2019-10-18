b3dd76e240a3e4e8801305a508f2ad259b35be39
Yasin <271155397@qq.com>
2019-10-19 00:08:43 +0800

    add Integer to Roman

--- src/main/java/medium/IntToRoman.java
+++ src/main/java/medium/IntToRoman.java
@@ -4,4 +4,19 @@ package medium;
  * @author Yasin Zhang
  */
 public class IntToRoman {
+    public String intToRoman(int num) {
+        int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder stringBuilder = new StringBuilder();
+        int index = 0;
+        while (index < 13) {
+            while (num >= nums[index]) {
+                stringBuilder.append(romans[index]);
+                num -= nums[index];
+            }
+            index++;
+        }
+        return stringBuilder.toString();
+    }
 }
