491fb925f0749a2aa9d08f5689596dbb20678a2d
yasin <271155397@qq.com>
2020-04-21 00:29:35 +0800

    add solution

--- src/main/java/medium/InorderTraversal.java
+++ src/main/java/medium/InorderTraversal.java
@@ -27,4 +27,22 @@ public class InorderTraversal {
 
         return result;
     }
+
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    public void helper(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            if (root.left != null) {
+                helper(root.left, res);
+            }
+            res.add(root.val);
+            if (root.right != null) {
+                helper(root.right, res);
+            }
+        }
+    }
 }
