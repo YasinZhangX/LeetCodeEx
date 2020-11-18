7679d0ce1d9c6c871bb93d05add318e4807a41fd
Yasin <271155397@qq.com>
2020-11-18 12:04:33 +0800

    add questions

---
+++ src/main/java/medium/CanCompleteCircuit.java
@@ -0,0 +1,24 @@
+package medium;
+
+/**
+ * 134. 加油站
+ * @author Yasin Zhang
+ */
+public class CanCompleteCircuit {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int len = gas.length;
+        int spare = 0;
+        int minSpare = Integer.MAX_VALUE;
+        int minIndex = 0;
+
+        for (int i = 0; i < len; i++) {
+            spare += gas[i] - cost[i];
+            if (spare < minSpare) {
+                minSpare = spare;
+                minIndex = i;
+            }
+        }
+
+        return spare < 0 ? -1 : (minIndex + 1) % len;
+    }
+}
